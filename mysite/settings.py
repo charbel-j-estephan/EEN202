62b71f15d07f1f25f03fa266a523166cb7012fd1
Charbel Estephan <charbelstephan12@gmail.com>
2024-05-06 00:02:23 +0300

    still need to fill in everything

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "products",
-    # "contact",
+    "contact",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
